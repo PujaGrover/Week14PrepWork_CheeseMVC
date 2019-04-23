9d5d78cacfb144ae4cd1c13905ebf04f16b00ff1
Puja Grover <puja_grover@yahoo.com>
2019-04-23 11:11:15 -0500

    Add project files.

---
+++ Week14PrepWork_CheeseMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week14PrepWork_CheeseMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
