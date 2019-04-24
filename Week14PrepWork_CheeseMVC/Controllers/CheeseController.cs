b5dde98a095b329a2c104f291b712666c1e42673
Puja Grover <puja_grover@yahoo.com>
2019-04-24 09:42:26 -0500

    Week14HwCheeseMVC: Tested, Sync, Commit, Push

---
+++ Week14PrepWork_CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week14PrepWork_CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            // Add the new cheese to my existing cheese
+            Cheeses.Add(name, description);
+
+            return Redirect("/Cheese");
+        }
+    }
+}
